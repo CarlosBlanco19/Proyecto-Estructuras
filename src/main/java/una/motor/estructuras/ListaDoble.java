a0c54efe892d90c22617f44506872c8216721bd4
Carlos <blancovargas19@gmail.com>
2026-08-26 15:22:05 -0600

    implementacion de lista, iterador y nodo

--- src/main/java/una/motor/estructuras/ListaDoble.java
+++ src/main/java/una/motor/estructuras/ListaDoble.java
@@ -4,6 +4,73 @@ import una.motor.estructuras.Nodo;
 
 public class ListaDoble<T> {
 
+    private Nodo<T> primero;
+    private Nodo<T> ultimo;
+    private int tamanio;
+
+    public ListaDoble(){
+        this.primero = null;
+        this.ultimo = null;
+        tamanio = 0;
+    }
+
+    private Nodo<T> getPosicion(int pos){
+        Nodo<T> temp = primero;
+        int i = 0;
+        while (i< pos){
+            temp = temp.getSiguiente();
+            i++;
+        }
+        return temp;
+    }
+    public boolean isEmpty(){
+        return primero ==null;
+    }
+
+
+
+    public void insertarIncio(T e){
+        Nodo<T> nuevo = new Nodo<T>(e);
+
+        if(isEmpty()){
+            primero = nuevo;
+            ultimo = nuevo;
+        }else{
+            nuevo.setSig(primero);
+            primero.setAnt(nuevo);
+            primero = nuevo;
+        }
+        tamanio ++;
+    }
+
+    public void insertarFinal(T e) {
+        Nodo<T> nuevo = new Nodo<T>(e);
+
+        if (isEmpty()) {
+            primero = nuevo;
+            ultimo = nuevo;
+        } else {
+            nuevo.setAnt(ultimo);
+            ultimo.setSig(nuevo);
+            ultimo = nuevo;
+        }
+        tamanio++;
+    }
+
+    public boolean eliminarPosicion(int pos){
+        if(pos<0 || pos >=tamanio){
+            return false;
+        }
+
+    }
+
+    public void eliminarElemento(T e){
+
+    }
+
+    public int getTamanio(){
+        return tamanio;
+    }
 
 
 }
--- src/main/java/una/motor/estructuras/Nodo.java
+++ src/main/java/una/motor/estructuras/Nodo.java
@@ -1,4 +1,40 @@
 package una.motor.estructuras;
 import una.motor.estructuras.Iterador;
-public class Nodo {
+
+public class Nodo<T> {
+
+    private T dato;
+    private Nodo<T> siguiente;
+    private Nodo<T> anterior;
+
+    public Nodo(T dato) {
+        this.dato = dato;
+        this.siguiente = null;
+        this.anterior = null;
+    }
+
+    public T getDato() {
+        return dato;
+    }
+
+    public Nodo<T> getAnterior(){
+        return anterior;
+    }
+
+    public Nodo<T> getSiguiente(){
+        return siguiente;
+    }
+
+    public void setDato(T dato){
+        this.dato = dato;
+    }
+
+    public void setSig(Nodo<T> sig){
+        this.siguiente = sig;
+    }
+
+    public void setAnt(Nodo<T> ant){
+        this.anterior = ant;
+    }
+
 }
